e9b6e6fa92c82b25215467b2398721b819564c56
fmohmedhussainal <fmohmedhussainal@paypal.com>
2018-04-09 19:24:41 -0700

    visual treatment for Signup page

--- app/components/signup/SignupComponent.jsx
+++ app/components/signup/SignupComponent.jsx
@@ -4,7 +4,7 @@ import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import RaisedButton from 'material-ui/RaisedButton';
 import TextField from 'material-ui/TextField';
 import Paper from 'material-ui/Paper';
-import { validateEmail } from '../../../lib/validation.js'
+import { isValidEmail } from '../../../lib/validation.js'
 
 import GiveLightLogoComponent from '../commonComponents/GiveLightLogoComponent'
 import VolunteerInterestsCheckboxesComponent from '../commonComponents/VolunteerInterestsCheckboxesComponent'
@@ -14,6 +14,8 @@ import { CountryDropdown, RegionDropdown } from 'react-country-region-selector-m
 
 import { registerUser, signupFacebook } from '../../api/api'
 import { interests } from '../../models/interests'
+import FontAwesome from 'react-fontawesome';
+import { render as _render } from "react-dom";
 
 
 require('../sharedCss.css')
@@ -38,6 +40,7 @@ class SignupComponent extends React.Component {
             retypePassphrase: '',
             isAdmin: false,
             checkboxInterests: checkInter,
+            errors: []
         }
     }
     handleField = (fieldName, event) => {
@@ -104,78 +107,96 @@ class SignupComponent extends React.Component {
         })
     }
     validateState = () => {
-        var errorMessage = ''
-        errorMessage = validateEmail(this.state.email)
+        let errors = [];
         if (this.state.name.length == 0) {
-            errorMessage += 'Please enter a valid name\n'
+            errors.push('Please enter a valid name.');
         }
-        if (!this.state.country) {
-            errorMessage += 'Please select a country\n'
-        }
-        if (!this.state.region) {
-            errorMessage += 'Please select a region\n'
+        if (!isValidEmail(this.state.email)) {
+            errors.push('Please enter a valid email.');
         }
         if (!this.state.passphrase) {
-            errorMessage += 'Please enter a passphrase\n'
+            errors.push('Please enter a passphrase.');
         }
         if (!this.state.retypePassphrase) {
-            errorMessage += 'Please retype the passphrase\n'
+            errors.push('Please retype the passphrase.');
         }
         if (this.state.passphrase !== this.state.retypePassphrase) {
-            errorMessage += 'Passphrases do not match\n'
+            errors.push('Passphrases do not match.');
         }
-        return errorMessage
+        if (!this.state.country) {
+            errors.push('Please select a country.');
+        }
+        if (!this.state.region) {
+            errors.push('Please select a region.');
+        }
+        return errors
     }
+
     handleSubmit(e) {
         e.preventDefault()
-        let error = this.validateState()
-        if (!error) {
+        let errors = this.validateState()
+        this.setState( {
+            ...this.state,
+            errors: errors
+        });
+        
+        if (errors.length == 0) {
             registerUser(this.state)
         }
-        else {
-            window.alert(error)
-        }
-
     }
+
+    renderErrors = () => {
+        let errors = this.state.errors;
+        if (errors.length != 0) {
+            const errorList = errors.map((error) => <li>{error}</li>);
+            return <div className="sign-up-error"><ul>{errorList}</ul></div>;
+        }
+    }
+
 
     render () {
         return (
             <Paper>
-            <GiveLightLogoComponent />
-            <form onSubmit={e => this.onSubmit(e)} className="main">
-                <div className="section">
-                    <button
-                        onClick={this.signupWithFacebook}
-                        className="uibutton"
-                    >
-                        &nbsp;&nbsp;&nbsp;&nbsp;Signup with Facebook&nbsp;&nbsp;&nbsp;&nbsp;
-                    </button>
-                    <div className="checkBoxStyle"><TextField type="text" name="name" value={this.state.name} floatingLabelText="Name" onChange={(e) => this.handleField('name', e)} /></div>
-                    <div><TextField type="text" name="email" value={this.state.email} floatingLabelText="Email" onChange={(e) => this.handleField('email', e)} /></div>
-                    <div><TextField type="number" floatingLabelText="Phone" name="phone" onChange={(e) => this.handleField('phone', e)} /></div>
-                    <div><TextField type="password" name="passphrase" value={this.state.passphrase} floatingLabelText="Passphrase" onChange={(e) => this.handleField('passphrase', e)} /></div>
-                    <div><TextField type="password" name="retypePassphrase" value={this.state.retypePassphrase} floatingLabelText="Retype Passphrase" onChange={(e) => this.handleField('retypePassphrase', e)} /></div>
-                    <div className="countryRegionContainer">
-                        <CountryDropdown
-                            value={this.state.country}
-                            onChange={this.handleCountry}
-                        />
-                    </div>
-                    <div>
-                        <RegionDropdown
-                            country={this.state.country}
-                            value={this.state.region}
-                            onChange={this.handleRegion}
-                        />
+                <div className="p-b-33">
+                    <div className="sign-up-panel-box p-l-30 p-r-30 p-t-10">
+                        <div className="sign-up-panel-fb-btn-container div-separator">
+                            <a href="#" onClick={this.signupWithFacebook} className="sign-up-fa-btn-face m-b-10">
+                                <FontAwesome name="facebook-official"/>Continue with Facebook
+                            </a>
+                        </div>
+                        <form onSubmit={e => this.onSubmit(e)} className="flex-sb flex-w">
+                            {this.renderErrors()}
+                            <div className="section">
+                                <div className="checkBoxStyle"><TextField type="text" name="name" value={this.state.name} floatingLabelText="Name" onChange={(e) => this.handleField('name', e)} /></div>
+                                <div><TextField type="text" name="email" value={this.state.email} floatingLabelText="Email" onChange={(e) => this.handleField('email', e)} /></div>
+                                <div><TextField type="number" floatingLabelText="Phone" name="phone" onChange={(e) => this.handleField('phone', e)} /></div>
+                                <div><TextField type="password" name="passphrase" value={this.state.passphrase} floatingLabelText="Passphrase" onChange={(e) => this.handleField('passphrase', e)} /></div>
+                                <div><TextField type="password" name="retypePassphrase" value={this.state.retypePassphrase} floatingLabelText="Retype Passphrase" onChange={(e) => this.handleField('retypePassphrase', e)} /></div>
+                                <div className="countryRegionContainer">
+                                    <CountryDropdown
+                                        value={this.state.country}
+                                        onChange={this.handleCountry}
+                                    />
+                                </div>
+                                <div className="countryRegionContainer">
+                                    <RegionDropdown
+                                        country={this.state.country}
+                                        value={this.state.region}
+                                        onChange={this.handleRegion}
+                                    />
+                                </div>
+                            </div>
+                            <div className={`section volunteerDetailsContainer`}>
+                                <h3>Please choose at most 3:</h3>
+                                <VolunteerInterestsCheckboxesComponent handleCheckbox={this.handleCheckbox} checkboxInterests={this.state.checkboxInterests} />
+                                <VolunteerSkillsInputComponent handleSkillsInput={this.handleSkillsInput} skillsInput={this.state.skillsInput ? this.state.skillsInput : ''} />
+                            </div>
+                            <div className="sign-up-panel-sign-up-btn-container m-t-17">
+                                <button onClick={e => this.handleSubmit(e)} className="sign-up-panel-sign-up-btn">Sign Up</button>
+                            </div>
+                        </form>
                     </div>
                 </div>
-                <div className={`section volunteerDetailsContainer`}>
-                    <h3>Please choose at most 3:</h3>
-                    <VolunteerInterestsCheckboxesComponent handleCheckbox={this.handleCheckbox} checkboxInterests={this.state.checkboxInterests} />
-                    <VolunteerSkillsInputComponent handleSkillsInput={this.handleSkillsInput} skillsInput={this.state.skillsInput ? this.state.skillsInput : ''} />
-                </div>
-                <div><button className="giveLightButton" onClick={e =>this.handleSubmit(e)} >sign up</button></div>
-            </form>
             </Paper>
         )
     }
